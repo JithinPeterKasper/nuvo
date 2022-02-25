e27c140597718d50b11a37b0ee9172ce0ce46c7c
JithinPeterKasper <jithin@techis.io>
2022-02-26 04:59:30 +0530

    Updated Button

--- nuvo/src/Main.jsx
+++ nuvo/src/Main.jsx
@@ -1,55 +1,63 @@
-import React, {useEffect, useState } from 'react'
-import Head from './components/Head'
-import Details from './components/Details'
-import API from './API'
+import React, { useEffect, useState } from "react";
+import Head from "./components/Head";
+import Details from "./components/Details";
+import API from "./API";
 
+import Select from "./components/Select";
 
 const api = new API();
 
-const Main =() => {
-    const [product, setProducts]= useState([]);
-    const [selected, setSelected]= useState(null);
+const Main = () => {
+  const [product, setProducts] = useState([]);
+  const [selected, setSelected] = useState(null);
+  
 
-    const limit=3
-    useEffect(()=>{
-        api
-         .fetchProduct({limit})
-         .then((response)=>{
-             setProducts(response.data);
-             setSelected(response.data[0].id)
-             console.log(response.data)
-         })
-    }, []);
+  const limit = 3;
+  useEffect(() => {
+    api.fetchProduct({ limit }).then((response) => {
+      setProducts(response.data);
+      setSelected(response.data[0].id);
+      console.log(response.data);
+    });
+  }, []);
 
-    return (
-        <div>
-            <Head/>
-            <div className="items">
-                <div className='info'>
-                {product.map((products) =>(
-                    <div className="select"
-                        key={products.id}
-                        onClick={() => setSelected(products.id)} >
-                            <img className="productImg" src={products.image} /><br />
-                            <p>{products.title}</p>
-                    </div>
-                ))}
-                </div>
-                <div className="details">
-                    {product.length> 0 && (
-                        <>
-                        {product.map((products)=> (
-                            <Details
-                            key={products.id}
-                            products={products}
-                            selected={selected} />
-                        ))}
-                        </>
-                    )}
-                </div>
-            </div>
+  
+
+  return (
+    <div>
+      <Head />
+      <div className="items">
+        <div className="info">
+          {product.map((products) => (
+            // <div
+            //   className="select"
+            //   key={products.id}
+            //   onClick={() => setSelected(products.id)}
+            // >
+            //   <img className="productImg" src={products.image} />
+            //   <br />
+            //   <p>{products.title}</p>
+            //   {arrow ? <img src={ArrowLeft}/> : <img src={ArrowRight}/>}
+            // </div>
+            <Select key={products.id} products={products} selected={selected} setSelected={setSelected}/>
+          ))}
         </div>
-    )
-}
+      <div className="details">
+          {product.length > 0 && (
+            <>
+              {product.map((products) => (
+                <Details
+                  key={products.id}
+                  products={products}
+                  selected={selected}
+                />
+              ))}
+            </>
+          )}
+        </div>
+        </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
--- nuvo/src/components/Select.jsx
+++ nuvo/src/components/Select.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import ArrowRight from "../assests/images/Arrow-Right.svg";
+import ArrowLeft from "../assests/images/Arrow-Left.svg";
 
-const Select=(products) =>{
-    return (
-        <div className="select">
-            
-        </div>
-    )
-}
+const Select = ({products, selected, setSelected}) => {
 
-export default Select
+    const [arrow, setArrow] = useState(true);
+    useEffect(() => {
+        if(products.id === selected){
+            setArrow(true);
+        }
+        else{
+            setArrow(false);
+        }
+    },[selected]);
+
+  return (
+    <div
+    className="select"
+    // key={products.id}
+    onClick={() => setSelected(products.id)}
+  >
+    <img className="productImg" src={products.image} />
+    <br />
+    <p>{products.title}</p>
+    {arrow ? <img src={ArrowLeft}/> : <img src={ArrowRight}/>}
+  </div>
+  );
+};
+
+export default Select;
